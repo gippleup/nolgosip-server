e813db924ec5624fe7ea77992107cf92aed1fb2e
monthem <lpoeh01@gmail.com>
2020-05-20 00:51:45 +0900

    add Sequelize Model: Vacation

---
+++ model/Vacation.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const vacation = sequelize.define(
+    'vacation', {
+      from: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      to: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: 'waiting',
+      },
+      approver: {
+        type: DataTypes.STRING,
+        defaultValue: 'admin',
+      },
+      reason: {
+        type: DataTypes.STRING,
+        defaultValue: '연차',
+      },
+    },
+    {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    },
+  );
+
+  vacation.associate = (models) => {};
+  return vacation;
+};
