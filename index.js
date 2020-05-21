1b114d8417755eb807b68595b4843c54ab71c708
monthem <lpoeh01@gmail.com>
2020-05-21 10:55:22 +0900

    add express middleware: cors, express-session, body-parser

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const router = require('./router');
+const config = require('./config');
+const db = require('./model/index');
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.end('OK');
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:5000'],
+  credentials: true,
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.db = db;
+  next();
 });
+
+app.use(session(config.session));
+
+app.use('/', router);
+
 
 app.listen(PORT, () => {
   console.log(`app started listening from PORT ${PORT}`);
