c7ef45db1deed6acc92814c887db231325544c12
monthem <lpoeh01@gmail.com>
2020-05-22 22:58:24 +0900

    add clear cookie for signout

--- controller/signout.js
+++ controller/signout.js
@@ -1,5 +1,6 @@
 module.exports = async (req, res) => {
-  if (!req.session.accessToken) res.json({ msg: 'no-session' });
+  if (!req.session.accessToken) res.status(400).json({ msg: 'no-session' });
   req.session.destroy();
+  res.clearCookie('accessToken');
   res.end('ok');
 };
